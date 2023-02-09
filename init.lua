631e36d5dcef19fd0ce4a4d89c5d69b32db15255
Nathan Fritz <nbfritz@gmail.com>
2023-02-09 15:22:54 -0600

    More tweaks

--- init.lua
+++ init.lua
@@ -1,38 +1,7 @@
 -- NeoVim Configuration
 
--- Helper Functions {{{
-
-function vim_commands(options)
-  for k,v in pairs(options) do
-    vim.cmd[k](v)
-  end
-end
-
-
-function set_options(options)
-  for k,v in pairs(options) do
-    vim.o[k] = v
-  end
-end
-
-function custom_commands(options)
-  for k,v in pairs(options) do
-    vim.api.nvim_create_user_command(k, v, { nargs = 0 })
-  end
-end
-
-function leader_keymaps(options)
-  for k,v in pairs(options) do
-    vim.keymap.set('n', '<Leader>' .. k, v, { silent = true })
-  end
-end
-
-function toggle(option)
-  vim.o[option] = not vim.o[option]
-end
--- }}}
-
 -- Source Other Files {{{
+vim.cmd("source ~/.config/nvim/lua/util.lua")
 vim.cmd("source ~/.config/nvim/lua/plugins.lua")
 -- }}}
 
